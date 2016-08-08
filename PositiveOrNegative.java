fd5749ef48753eafc19d1d7647f84c801e290079
Saravanakumarcse <manosaravanan31@gmail.com>
2016-08-08 12:08:17 +0530

    Create PositiveOrNegative.java

---
+++ PositiveOrNegative.java
@@ -0,0 +1,15 @@
+class PositiveOrNegative{
+int num=12;
+if(num<0){
+System.out.println("Positive");
+}
+else if(num>0){
+System.out.println("Negative");
+}
+else if(num==0){
+System.out.println("Zero number");
+}
+else{
+System.out.println("Number is Invalid");
+}
+}
