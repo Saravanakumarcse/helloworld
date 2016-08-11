66dce444f496773c4e4ea7545df3631a974f976d
Saravanakumarcse <manosaravanan31@gmail.com>
2016-08-11 12:13:33 +0530

    Create OddNumEx.java

---
+++ OddNumEx.java
@@ -0,0 +1,11 @@
+class OddNumEx
+{
+int i=10;
+//System.out.println("Enter given number ");
+//Scanner in=new Scanner(System.in);
+//odd=in.nextInt();
+for(int i=10;i<=20;i++)
+{
+System.out.println("odd"+i);
+}
+}
