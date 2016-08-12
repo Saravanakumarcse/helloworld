9b89d8b28db7b4a805ec48f3adeb4a9f04572149
Saravanakumarcse <manosaravanan31@gmail.com>
2016-08-12 09:52:31 +0530

    Create Amstrong.java

---
+++ Amstrong.java
@@ -0,0 +1,19 @@
+class Amstrong
+{
+int a=0;
+int n=153;
+int temp=n;
+if(temp>n)
+{
+int b=n%10;
+a=a+(b*b*b);
+}
+while(temp==n)
+{
+System.out.println("Amstrong");
+}
+else
+{
+System.out.println("Not Amstrong");
+}
+}
